21e45942fb9e1ca71b9c2422b8ed9f4cce831403
salam <salamatbekboev2008@gmail.com>
2026-03-07 05:36:39 +0900

    feat(widget): margin and padding boxes for widget

--- game/widgets/__init__.py
+++ game/widgets/__init__.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass
 from enum import Enum
-from typing import Generic, List, Optional, Tuple, Type, TypeVar
+from typing import Any, Dict, Generic, List, Optional, Tuple, Type, TypeVar
 
 from pygame import Rect, Surface, draw
 
@@ -41,9 +40,9 @@ class Widget:
 
     rect: Rect
     parent: Rect
+    margin: Rect
+    padding: Rect
     childs: List[Widget]
-    margin: int
-    padding: int
     align: Optional[Align] = None
     background: Optional[ColorValue]
     outline: Optional[tuple[ColorValue, int]]
@@ -53,48 +52,68 @@ class Widget:
         self,
         rect: Rect,
         parent: Rect,
+        margin: Rect,
+        padding: Rect,
         childs: List[Widget],
         align: Optional[Align],
-        margin: int = 1,
-        padding: int = 0,
         background: Optional[ColorValue] = None,
         outline: Optional[tuple[ColorValue, int]] = None,
     ) -> None:
         self.rect = rect
         self.parent = parent
         self.childs = childs
+        if margin:
+            self.margin = margin
+        else:
+            self.margin = rect
+        if padding:
+            self.padding = padding
+        else:
+            self.padding = rect
         if align:
-            self.set_align(align)
-        self.margin = margin
-        self.padding = padding
+            self.align = align
         self.background = background
         self.outline = outline
 
-    def set_align(self, align: Align):
-        self.align = align
+    def __setattr__(self, name: str, value: Any, /) -> None:
+        super().__setattr__(name, value)
+        if name == "align":
+            self.realign()
+
+    def realign(self):
+        deltax = self.margin.centerx - self.rect.centerx
+        deltay = self.margin.centery - self.rect.centery
+        deltax2 = self.margin.centerx - self.padding.centerx
+        deltay2 = self.margin.centery - self.padding.centery
         match self.align:
             case Align.CENTER:
-                self.rect.center = self.parent.center
+                self.margin.center = self.parent.center
             case Align.TOP:
-                self.rect.top = self.parent.top
-                self.rect.centerx = self.parent.centerx
+                self.margin.midtop = self.parent.midtop
             case Align.BOTTOM:
-                self.rect.bottom = self.parent.bottom
-                self.rect.centerx = self.parent.centerx
+                self.margin.midbottom = self.parent.midbottom
             case Align.LEFT:
-                self.rect.left = self.parent.left
-                self.rect.centery = self.parent.centery
+                self.margin.midleft = self.parent.midleft
             case Align.RIGHT:
-                self.rect.right = self.parent.right
-                self.rect.centery = self.parent.centery
+                self.margin.midright = self.parent.midright
             case Align.TOPLEFT:
-                self.rect.topleft = self.parent.topleft
+                self.margin.topleft = self.parent.topleft
             case Align.BOTTOMLEFT:
-                self.rect.bottomleft = self.parent.bottomleft
+                self.margin.bottomleft = self.parent.bottomleft
             case Align.TOPRIGHT:
-                self.rect.topright = self.parent.topright
+                self.margin.topright = self.parent.topright
             case Align.BOTTOMRIGHT:
-                self.rect.bottomright = self.parent.bottomright
+                self.margin.bottomright = self.parent.bottomright
+        self.rect.center = (
+            self.margin.centerx + deltax,
+            self.margin.centery + deltay,
+        )
+        self.padding.center = (
+            self.margin.centerx + deltax2,
+            self.margin.centery + deltay2,
+        )
+        for child in self.childs:
+            child.realign()
 
     def set_outline(self, color: ColorValue, width: int):
         self.outline = (color, width)
@@ -102,15 +121,14 @@ class Widget:
     def set_background(self, color: ColorValue):
         self.background = color
 
-    def focus(self, pos: Optional[tuple[int, int]] = None):
+    def focus(self, pos: tuple[int, int]):
         """
         Sets widget focused if given point collides widget rect
         """
-        if pos and self.rect:
-            self.focused = self.rect.collidepoint(*pos)
-            if self.focused:
-                for child in self.childs:
-                    child.focus(pos)
+        self.focused = self.rect.collidepoint(*pos)
+        if self.focused:
+            for child in self.childs:
+                child.focus(pos)
 
     def draw(self, screen: Surface):
         "Draws widget and his childs on given surface"
@@ -125,7 +143,10 @@ class Widget:
 WidgetType = TypeVar("WidgetType", bound=Widget)
 
 
-@dataclass
+def El(cls: Type[WidgetType], *args, **kwargs) -> WidgetBuilder[WidgetType]:
+    return WidgetBuilder(cls, *args, **kwargs)
+
+
 class WidgetBuilder(Generic[WidgetType]):
     """
     Generic widget builder
@@ -164,34 +185,52 @@ class WidgetBuilder(Generic[WidgetType]):
 
     widget_class: Type[WidgetType]
     rect: Optional[Rect] = None
+    margin: Optional[Rect] = None
+    padding: Optional[Rect] = None
     childs: Optional[List[WidgetBuilder]] = None
     align: Optional[Align] = None
     background: Optional[ColorValue] = None
     outline: Optional[Tuple[ColorValue, int]] = None
+    kwargs: Dict
 
-    def with_rect(self, rect):
+    def __init__(
+        self,
+        widget_class: Type[WidgetType],
+        rect: Optional[Rect] = None,
+        margin: Optional[Rect] = None,
+        padding: Optional[Rect] = None,
+        childs: Optional[List[WidgetBuilder]] = None,
+        align: Optional[Align] = None,
+        background: Optional[ColorValue] = None,
+        outline: Optional[Tuple[ColorValue, int]] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__()
+        self.widget_class = widget_class
         self.rect = rect
-
-    def with_childs(self, childs):
+        self.margin = margin
+        self.padding = padding
         self.childs = childs
-
-    def with_align(self, align):
         self.align = align
-
-    def with_background(self, background):
         self.background = background
-
-    def with_outline(self, outline):
         self.outline = outline
+        self.kwargs = kwargs
 
     def build(self, parent: Rect):
         rect = self.rect if self.rect else parent
-        childs = [child.build(rect) for child in self.childs] if self.childs else []
+        margin = self.margin if self.margin else rect
+        content_rect = self.padding if self.padding else rect
+        childs = (
+            [child.build(content_rect) for child in self.childs] if self.childs else []
+        )
         return self.widget_class(
             rect=rect,
             parent=parent,
+            margin=margin,
+            padding=content_rect,
             childs=childs,
             align=self.align,
             background=self.background,
             outline=self.outline,
+            **self.kwargs,
         )
